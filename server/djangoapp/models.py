6df44a64ab048e65197a24108ee4386becdfa168
Oleksii Hlobets <aleksey_g@bigmir.net>
2022-06-30 16:01:02 +0000

    New models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,21 +4,47 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'wagon'
+    HATCHBACK = 'hatchback'
+    CROSSOVER = 'crossover'
+    CONVERTIBLE = 'convertible'
+    COUPE = 'coupe'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (HATCHBACK, 'Hatchback'),
+        (CROSSOVER, 'Crossover'),
+        (CONVERTIBLE, 'Convertible'),
+        (COUPE, 'Coupe')
+    ]
+    id = models.AutoField(primary_key=True)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    dealer = models.IntegerField(default=0)
+    car_type = models.CharField(
+        null=False,
+        max_length=15,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+    car_year = models.DateField(null=True)
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Type: " + self.car_type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
